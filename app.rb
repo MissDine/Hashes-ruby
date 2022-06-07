9908d902dd024a30e13e5a601d94e5727db77577
MissDine <enidnyatichi1212@gmail.com>
2022-06-07 22:41:25 +0300

    Feature edit

---
+++ app.rb
@@ -0,0 +1,22 @@
+#  hashes also hold varioustypes of data just like arrays
+# Hashes contain key-value pairs
+# Hashes are more of objects in javascript
+# syntax of hashes
+ hash_name = { "key" => "value" }
+
+
+hash_name = {
+    "key1" => 1,
+    "key2" => 2,
+    "key3" => 3.7,
+    "key4" => "Enid"
+}
+puts hash_name["key4"]
+# Creating a hash
+fruits = {
+    "green" => "apple",
+    "yellow" => "banana",
+    "green" => "guava"
+    
+}
+puts fruits["yellow"]
